55667eaaaf79262420e8180ed0963e79595d6d8f
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-06 23:26:16 -0300

    Update skilltree.js

--- skilltree.js
+++ skilltree.js
@@ -1,7 +1,9 @@
-document.getElementById("checkMark").addEventListener("click", function() {
-  if (this.innerHTML === "⬜️") {
-    this.innerHTML = "⚪️";
-  } else {
-    this.innerHTML = "⬜️";
-  }
+document.getElementById("openBtn").addEventListener("click", function() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+});
+
+document.getElementById("closeBtn").addEventListener("click", function() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 });
