3998a74f8db99551e3766cbea42543e9af81af80
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-06 21:55:01 -0300

    Create skilltree.js

---
+++ skilltree.js
@@ -0,0 +1,7 @@
+document.getElementById("checkMark").addEventListener("click", function() {
+  if (this.innerHTML === "✔️") {
+    this.innerHTML = "⬜️";
+  } else {
+    this.innerHTML = "✔️";
+  }
+});
