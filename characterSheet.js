f060374c0533100b7b32cc1b4842890495687f23
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-09 19:58:37 -0300

    Update characterSheet.js

--- characterSheet.js
+++ characterSheet.js
@@ -745,3 +745,15 @@ hamburger.addEventListener('click', ()=>{
   button.addEventListener('click', function() {
     window.location.href = 'https://reysouza.github.io/Akari/';
   });
+
+ function toggleNav() {
+        var sidebar = document.getElementById("mySidebar");
+        var main = document.getElementById("main");
+        if (sidebar.style.width === "250px") {
+          sidebar.style.width = "0";
+          main.style.marginLeft = "0";
+        } else {
+          sidebar.style.width = "250px";
+          main.style.marginLeft = "250px";
+        }
+      }
